f1e11c699b788284f2741f0551723c45aa825013
Monu0422 <monu010715@gmail.com>
2024-07-19 09:15:23 +0530

    Update pyroplug.py

--- ggn/assets/pyroplug.py
+++ ggn/assets/pyroplug.py
@@ -606,7 +606,7 @@ async def set_rename_command(user_id, custom_rename_tag):
 # Function to get the user's custom renaming preference
 def get_user_rename_preference(user_id):
     # Retrieve the user's custom renaming tag if set, or default to '@devggn'
-    return user_rename_preferences.get(str(user_id), '@The_real_xTaR')
+    return user_rename_preferences.get(str(user_id), '@devggn')
 
 # Function to set custom caption preference
 async def set_caption_command(user_id, custom_caption):
@@ -633,7 +633,7 @@ async def settings_command(event):
         [Button.inline("Remove Words", b'delete')],
         [Button.inline("Login", b'addsession'), Button.inline("Logout", b'logout')],
         [Button.inline("Set Thumbnail", b'setthumb'), Button.inline("Remove Thumbnail", b'remthumb')],
-        [Button.url("Report Errors", "https://t.me/xTaR_Main")]
+        [Button.url("Report Errors", "https://t.me/devggn")]
     ]
     
     await gf.send_file(
